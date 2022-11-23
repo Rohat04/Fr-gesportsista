9c5aa471103045c5f50ddcad2df2d14af6585310
Rohat04 <ozkocrohat@gmail.com>
2022-11-23 14:19:18 +0100

    Frågesportsista

---
+++ Frågesportsista/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Frågesportsista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
